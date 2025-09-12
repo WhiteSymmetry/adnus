e80014e29bb484aee7990cf792c8ca9f51c91546
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-09-12 19:05:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,41 @@
 import io
 import re
 from setuptools import setup, find_packages
+import os
 import sys
 
+def get_version():
+    """_version.py dosyasından versiyon numarasını al"""
+    version_file = os.path.join(os.path.dirname(__file__), 'src', 'adnus', '_version.py')
+    
+    try:
+        with open(version_file, 'r') as f:
+            version_content = f.read()
+            version_match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", 
+                                    version_content, re.M)
+            if version_match:
+                return version_match.group(1)
+    except FileNotFoundError:
+        pass
+    
+    # Fallback: __init__.py'den versiyon almayı dene
+    init_file = os.path.join(os.path.dirname(__file__), 'src', 'adnus', '__init__.py')
+    try:
+        with open(init_file, 'r') as f:
+            init_content = f.read()
+            version_match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", 
+                                    init_content, re.M)
+            if version_match:
+                return version_match.group(1)
+    except FileNotFoundError:
+        pass
+    
+    # Hiçbiri yoksa varsayılan versiyon
+    return "0.1.0"
 
 setup(
     name="adnus",
-    version="0.1.5",
+    version=get_version(),
     description="adnus",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -16,13 +45,13 @@ setup(
     author_email="mkececi@yaani.com",
     maintainer_email="mkececi@yaani.com",
     url="https://github.com/WhiteSymmetry/adnus",
-    packages=find_packages(),  # 'adnus' klasörünü otomatik bulur
+    packages=find_packages(where='src'),  # src klasöründeki paketleri bul
+    package_dir={'': 'src'},  # Paketlerin src altında olduğunu belirt
     package_data={
-        "adnus": ["__init__.py", "_version.py"]  # Gerekli dosyaları dahil et
+        "adnus": ["*.py", "*.pyi"],  # Tüm Python dosyalarını dahil et
     },
     install_requires=[
         "hypercomplex",
-
     ],
     extras_require={
         'test': [
