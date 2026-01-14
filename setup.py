abb80aa9c139cb739a435f60225b1e60201a96ee
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-01-14 17:28:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ def get_version():
                 if line.startswith('__version__'):
                     return line.split('=')[1].strip().strip('"\'')
     except FileNotFoundError:
-        return "0.1.7"
-    return "0.1.7"
+        return "0.1.8"
+    return "0.1.8"
 
 setup(
     name="adnus",
