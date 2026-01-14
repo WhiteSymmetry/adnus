82c15a4537808949f4cf6abec44b6dfc69447591
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-01-14 17:27:03 +0300

    Update _version.py

--- src/adnus/_version.py
+++ src/adnus/_version.py
@@ -1,7 +1,7 @@
 # _version.py
 
-__version__ = "0.1.7"
-__license__ = "MIT"
+__version__ = "0.1.8"
+__license__ = "AGPL"
 __description__ = "adnus (AdNuS):  Advanced Number Systems."
 __author__ = "Mehmet Keçeci"
 __url__ = "https://github.com/WhiteSymmetry/adnus"
